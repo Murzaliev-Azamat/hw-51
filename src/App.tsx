946afc239459903b87de1c92a61f48686d4faa58
Azamat Murzaliev <lirog.kg@gmail.com>
2022-10-21 17:02:55 +0600

    Сделал компонент Ball

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Ball from "./Ball/Ball";
+
 
 function App() {
+  const [number, setNumber] = useState([
+
+    ]);
+  const changeNumber = () => {
+    console.log("clicked")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <button onClick={changeNumber}>New numbers</button>
+      <Ball number={Math.floor(Math.random() * 32) + 5}/>
     </div>
   );
 }
---
+++ src/Ball/Ball.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface BallProps {
+  number: number;
+}
+
+const Ball: React.FC<BallProps> = props => {
+  return (
+    <div className="ball">
+      {props.number}
+
+    </div>
+  );
+};
+
+export default Ball;
